dc653f16524a02e422e5a8587f29c14f8731ae43
Kaviyarasiii <136569304+Kaviyarasiii@users.noreply.github.com>
2024-07-10 23:48:39 +0530

    Add files via upload

---
+++ WholeWords.java
@@ -0,0 +1,13 @@
+
+public class WholeWords {
+
+	public static void main(String[] args) {
+		char[] ch= {'D','e','v','l','o','p','e','r'};
+		//convert array of characters into String
+		String s=String.valueOf(ch);
+		System.out.println(s);
+				
+		}
+	}
+
+//String s=new String(ch);
